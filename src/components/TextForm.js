51326410514f87a3207b72204eef8fdc2e1a42ca
Ashish Singh <ashishsingh.9x@gmail.com>
2024-07-23 00:30:34 +0530

    more logic in textutil

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -14,8 +14,9 @@ export default function TextForm(props) {
     const [text, setText] = useState("Enter text here");
 
     return (
-        <div>
-            <h1>{props.heading} {text}</h1>
+        <>
+        <div className="container">
+            <h1>{props.heading}</h1>
             <div className="form-group">
                 <label htmlFor="exampleFormControlTextarea1">Example textarea</label>
                 <textarea
@@ -26,5 +27,13 @@ export default function TextForm(props) {
             </div>
             <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
         </div>
+        <div className="container my-3">
+            <h1>Your text summary</h1>
+            <p>{text.split(" ").length} words and {text.length} characters</p>
+            <p>{0.008 * text.split(" ").length} minutes read</p>
+            <p>Preview</p>
+            {text}
+        </div>
+        </>
     );
 }
